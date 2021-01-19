ba91401e5d2bfa0e130dbfdc4b3f47b994f0a27e
nomaldog <chuken717@gmail.com>
2021-01-19 23:12:19 +0900

    material name => vue.js+json

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
     var app = new Vue({
     el: '#app',
     data: {
-       items:null
+       characters:null,
+       selected:"なし"
     },
     mounted: function () {
-      axios.get("./character.json").then(response => (this.items = response.data))
+      axios.get("./character.json").then(response => (this.characters = response.data))
     }
   })
 })
