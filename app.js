d917afaedbbf6dae40335a977c2c58f919b6322c
nomaldog <chuken717@gmail.com>
2021-01-20 21:40:36 +0900

    use computed property

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
        characters:null,
        selected:"なし"
     },
+    computed: {
+      selected_character: function () {
+        return this.characters.find((v) => v.name === this.selected);
+      }
+    },
     mounted: function () {
       axios.get("./character.json").then(response => (this.characters = response.data))
     }
