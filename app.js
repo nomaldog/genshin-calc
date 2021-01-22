04b1dad084847e001fdd00337b6e34973e10df0e
nomaldog <chuken717@gmail.com>
2021-01-22 17:12:24 +0900

    I used "vue.js+json" for all materials caluclate

--- app.js
+++ app.js
@@ -1,105 +1,72 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener('DOMContentLoaded', function(event) {
   var app = new Vue({
     el: '#app',
     data: {
-      // json
       characters:null,
       exp_table:null,
-
-      // input
-      selected:"なし",
-      input_org:1,
-      input_nxt:80,
-      input_break_org:false,
-      input_break_nxt:false,
-      // result
-      exp_star4:0 ,
-      exp_star3:0 ,
-      exp_star2:0 ,
-      mora:0,
-      local_material:0,
-      star1_material:0,
-      star2_material:0,
-      star3_material:0
+      input:{
+        selected:'なし',
+        org:1,
+        nxt:80,
+        break_org:false,
+        break_nxt:false
+      },
+      result:{exp_book4:0 ,
+              exp_book3:0 ,
+              exp_book:0 ,
+              mora:0,
+              local_material:0,
+              demon1_material:0,  
+              demon2_material:0,
+              demon3_material:0,
+              elite_material:0,
+              elem2_material:0,
+              elem3_material:0,
+              elem4_material:0,
+              elem5_material:0}
     },
     computed: {
       selected_character: function () {
-        return this.characters ? this.characters.find((v) => v.name === this.selected) : {"name": "なし",  "element": "なし",  "local_material": "特産品",  "star1_material": "通常素材(★1)",  "star2_material": "通常素材(★2)",  "star3_material": "通常素材(★3)"};
+        return this.characters ? this.characters.find((v) => v.name === this.input.selected) : {'name': 'なし',  'element': 'なし',  'local_material': '特産品',  'star1_material': '魔物素材(★1)',  'star2_material': '魔物素材(★2)',  'star3_material': '魔物素材(★3)'};
       },
       calc_exp: function(){
-        if(this.exp_table && this.characters){
-          // 経験値テーブルの検索
-          const nxt_row = this.exp_table.find((v) => v.level == Number(this.input_nxt));
-          const org_row = this.exp_table.find((v) => v.level == Number(this.input_org));
-
-          // 経験値本の計算
+        if(this.exp_table && this.characters && this.input.org && this.input.nxt){
+          const nxt_row = this.exp_table.find((v) => v.level == Number(this.input.nxt));
+          const org_row = this.exp_table.find((v) => v.level == Number(this.input.org));
+          
           const result_exp = nxt_row.total_exp - org_row.total_exp;
-          this.exp_star4 = Math.ceil(result_exp / 20000); // 星4経験値本：必要数
-          this.exp_star3 = Math.ceil(result_exp /  5000); // 星3経験値本：必要数
-          this.exp_star2 = Math.ceil(result_exp /  1000); // 星2経験値本：必要数
+          this.result.exp_book4 = Math.ceil(result_exp / 20000);
+          this.result.exp_book3 = Math.ceil(result_exp /  5000);
+          this.result.exp_book2 = Math.ceil(result_exp /  1000);
 
-          // モラの計算
           const break_point = [20,40,50,60,70,80];
           let result_mora = nxt_row.total_mora - org_row.total_mora;
           break_point.forEach(function(bp,index){
-            if(this.input_break_nxt && bp == this.input_nxt) result_mora += 20000 * (index + 1);
-            if(this.input_break_org && bp == this.input_org) result_mora -= 20000 * (index + 1);
+            if(this.input.break_nxt && bp == Number(this.input.nxt)) result_mora += 20000 * (index + 1);
+            if(this.input.break_org && bp == Number(this.input.org)) result_mora -= 20000 * (index + 1);
           }, this);
-          this.mora = result_mora.toLocaleString('ja-JP');
+          this.result.mora = result_mora.toLocaleString('ja-JP');
         }
         return null;
       },
       calc_material: function(){
-        // 必要素材カウント(0:特産, 1:魔物星1, 2:魔物星2, 3:魔物星3, 4:精鋭, 5:属性星2, 6:属性星3, 7:属性星4, 8:属性星5) * 現在値・目標値
-        let need_mat = [[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]];
-        for (let i = 0; i < 2; i++) {
-          let tgt_lv = (i==0 ? this.input_org + (this.input_break_org ? 1 : 0) : this.input_nxt + (this.input_break_nxt ? 1 : 0));
-          if(tgt_lv > 20){
-            need_mat[0][i] += 3;
-            need_mat[1][i] += 3;
-            need_mat[5][i] += 1;
-          }
-          if(tgt_lv > 40){
-            need_mat[0][i] += 10;
-            need_mat[1][i] += 15;
-            need_mat[4][i] += 2;
-            need_mat[6][i] += 3;
-          }
-          if(tgt_lv > 50){
-            need_mat[0][i] += 20;
-            need_mat[2][i] += 12;
-            need_mat[4][i] += 4;
-            need_mat[6][i] += 6;
-          }
-          if(tgt_lv > 60){
-            need_mat[0][i] += 30;
-            need_mat[2][i] += 18;
-            need_mat[4][i] += 8;
-            need_mat[7][i] += 3;
-          }
-          if(tgt_lv > 70){
-            need_mat[0][i] += 45;
-            need_mat[3][i] += 12;
-            need_mat[4][i] += 12;
-            need_mat[7][i] += 6;
-          }
-          if(tgt_lv > 80){
-            need_mat[0][i] += 60;
-            need_mat[3][i] += 24;
-            need_mat[4][i] += 20;
-            need_mat[8][i] += 6;
-          }
-        }
-        this.local_material = need_mat[0][1] - need_mat[0][0];
-        this.star1_material = need_mat[1][1] - need_mat[1][0];
-        this.star2_material = need_mat[2][1] - need_mat[2][0];
-        this.star3_material = need_mat[3][1] - need_mat[3][0];
+        const nxt_row = this.exp_table.find((v) => v.level == Number(this.input.nxt) + this.input.break_nxt ? 1 : 0);
+        const org_row = this.exp_table.find((v) => v.level == Number(this.input.org) + this.input.break_org ? 1 : 0);
+        this.result.local_material = nxt_row.local - org_row.local;
+        this.result.demon1_material = nxt_row.demon1 - org_row.demon1;
+        this.result.demon2_material = nxt_row.demon2 - org_row.demon2;
+        this.result.demon3_material = nxt_row.demon3 - org_row.demon3;
+        this.result.elite_material = nxt_row.elite - org_row.elite;
+        this.result.elem2_material = nxt_row.elem2 - org_row.elem2;
+        this.result.elem3_material = nxt_row.elem3 - org_row.elem3;
+        this.result.elem4_material = nxt_row.elem4 - org_row.elem4;
+        this.result.elem5_material = nxt_row.elem5 - org_row.elem5;
         return null;
       }
     },
     mounted: function () {
-      axios.get("./characters.json").then(response => (this.characters = response.data));
-      axios.get("./exp_table.json").then(response => (this.exp_table = response.data));
+      axios.get('./characters.json').then(response => (this.characters = response.data));
+      axios.get('./exp_table.json').then(response => (this.exp_table = response.data));
     }
   })
 })
