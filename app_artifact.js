ad6d793f4bb726508b49fa6ca35796cde7d74155
nomaldog <chuken717@gmail.com>
2021-01-29 23:13:19 +0900

    Updated artifact to beta. Temporary implementation(Lv90 unexplored region orbit)

--- app_artifact.js
+++ app_artifact.js
@@ -4,15 +4,20 @@ document.addEventListener('DOMContentLoaded', function(event) {
     data: {
       exp_table:null,
       rarity_list:[1,2,3,4,5],
+      part_list:[1,2,3,4,5],
       level_list:[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
       input: {
         rarity:5,
         before:0,
-        after:20
+        after:20,
+        parts:1
       },
       result: {
         artifacts:[0,0,0,0,0],
         mora:0,
+        orbit:0,
+        resin:0,
+        days:[0,0,0,0,0],
         message:'',
         message_style:''
       }
@@ -34,13 +39,23 @@ document.addEventListener('DOMContentLoaded', function(event) {
 
         const before_exp = this.exp_table.find((v) => v.level == this.input.before && v.rarity == this.input.rarity).total_exp;
         const after_exp  = this.exp_table.find((v) => v.level == this.input.after  && v.rarity == this.input.rarity).total_exp;
-        const need_exp = after_exp - before_exp;
+        const need_exp = (after_exp - before_exp) * this.input.parts;
         const get_exp = [3780,2520,1260,840,420];
         
         this.result.artifacts.forEach(function(artifact,index){
           this.result.artifacts[index] = Math.ceil(need_exp / get_exp[index]);
         },this);
         this.result.mora = need_exp.toLocaleString('ja-JP');
+
+        const exp_per_orbit = 14742; // 概算値
+        const dairy_recovery = 180;
+        const consumption_list = [0,1,3,4,6];
+        this.result.orbit = Math.ceil(need_exp / exp_per_orbit);
+        this.result.resin = this.result.orbit * 20;
+        
+        consumption_list.forEach(function(consumption,index){
+          this.result.days[index] = (Math.ceil(this.result.resin / (dairy_recovery +  60 * consumption) * 100) / 100) + '日';
+        },this);
       }
     },
     mounted: function () {
